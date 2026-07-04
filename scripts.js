4224d0ef061dc287703719c41b8c5c74a353d104
MasterRoachi <207052285+MasterRoachi@users.noreply.github.com>
2026-07-04 23:35:04 +0200

    Figured out the button
    I fixed the bug in the button

--- scripts.js
+++ scripts.js
@@ -1,40 +1,40 @@
-const container = document.querySelector(".container")
+const container = document.querySelector(".container");
+
+let size = 0;
 
 function draw() {
-    const squares = document.querySelectorAll(".block")
-    squares.forEach(square => {
-    square.addEventListener('mouseover', () => {
-        square.style.backgroundColor = "#d9a441"
-    })
-})
+  const squares = document.querySelectorAll(".block");
+  squares.forEach((square) => {
+    square.addEventListener("mouseover", () => {
+      square.style.backgroundColor = "#d9a441";
+    });
+  });
 }
 
 function createGrid(gridHeight, gridWidth) {
-    for (let i = 1; i <= gridHeight * gridWidth; i++) {
-        const gridBlock = document.createElement("div")
-        gridBlock.classList.add('block')
+  for (let i = 1; i <= gridHeight * gridWidth; i++) {
+    const gridBlock = document.createElement("div");
+    gridBlock.classList.add("block");
 
-        gridBlock.style.width = `${100 / gridWidth}%`
-        gridBlock.style.height = `${100 / gridHeight}%`
+    gridBlock.style.width = `${100 / gridWidth}%`;
+    gridBlock.style.height = `${100 / gridHeight}%`;
 
-        container.appendChild(gridBlock)
-    }
-    draw()
+    container.appendChild(gridBlock);
+  }
+  draw();
 }
 
-createGrid(16,16)
-
+createGrid(16, 16);
 
 const refreshButton = document.querySelector("#refresh");
-refreshButton.addEventListener('click',(refresh) => {
-    document.querySelectorAll('.block').forEach(gridBlock => {
-        gridBlock.remove();
-    });
-    let size = Number(prompt("How many squares per side?"));
-     if (size > 100) {
-        let size = Number(prompt("That's too much!"))
-    } else {
-    createGrid(size,size)
-    draw()
-})
-
+refreshButton.addEventListener("click", (refresh) => {
+  document.querySelectorAll(".block").forEach((gridBlock) => {
+    gridBlock.remove();
+  });
+  let size = Number(prompt("How many squares per side?"));
+  while (size > 100) {
+    size = Number(prompt("That's too much!"));
+  }
+    createGrid(size, size);
+    draw();
+});
